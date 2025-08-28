7bdabf73511469a161e65b2fb3c4439f41b3b6c2
mehrankhantt <cloud.mehrankhan@gmail.com>
2025-08-28 21:41:37 +0500

    testing 1ss

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ resource "aws_s3_bucket_public_access_block" "logs" {
 
   block_public_acls       = false
   block_public_policy     = true
-  ignore_public_acls      = true
+  ignore_public_acls      = false
   restrict_public_buckets = true
 }
 
