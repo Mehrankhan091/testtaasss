35913f1f08085565c2286f022b097ee0665c7150
mehrankhantt <cloud.mehrankhan@gmail.com>
2025-08-28 16:13:54 +0500

    Terraform Injected

--- terraform/main.tf
+++ terraform/main.tf
@@ -155,8 +155,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "none"
-      locations        = ["none"]
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
     }
   }
 
